43f0d7a86f53e8f28e96eccfce30184331921545
Erlend Westbye <erlendwestbye@gmail.com>
2014-09-23 15:47:38 +0200

    Lagt til border istedet for drop shadow

--- funksjoner.js
+++ funksjoner.js
@@ -4,6 +4,18 @@
 function endreInnhold(a){
 
 
+    if(a== 0){
+
+        document.getElementById('endres').className = 'test';
+        document.getElementById('spacer').style.height= 300;
+    }
+
+    if(a== 1){
+
+        document.getElementById('endres').className = 'top_block';
+        document.getElementById('spacer').style.height= 0;
+
+    }
 
 
 
@@ -15,7 +27,7 @@ function endreInnhold(a){
 
 function onScroll(){
 
-    var lengdefratopp = document.body.scrollTop
+    var lengdefratopp = document.body.scrollTop;
 
     if(lengdefratopp>150){
 
