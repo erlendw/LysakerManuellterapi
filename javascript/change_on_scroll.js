65e2d49d07fceccd18ed708899b6c2a19c2e6c80
Erlend Westbye <erlendwestbye@gmail.com>
2014-10-17 19:31:21 +0200

    Fikset en en del ting. LAgt inn tekst og fasilitert for tekstanimasjoner

--- javascript/change_on_scroll.js
+++ javascript/change_on_scroll.js
@@ -17,8 +17,11 @@ function endreID(el){
 
         document.getElementById('js_top_block').className = 'top_block_js';
         document.getElementById('js_mid_block').className = 'mid_block_js';
+        document.getElementById('js_top_block_text').className = 'top_block_text_js';
+        document.getElementById('js_site_title').className = 'site_title_js';
 
-        fadein('js_top_block')
+
+        fadein('js_top_block');
 
         sjekk = 1;
 
@@ -28,6 +31,8 @@ function endreID(el){
 
         document.getElementById('js_top_block').className = 'top_block';
         document.getElementById('js_mid_block').className = 'mid_block';
+        document.getElementById('js_top_block_text').className = 'top_block_text';
+        document.getElementById('js_site_title').className = 'site_title';
 
         sjekk = 0;
     }
